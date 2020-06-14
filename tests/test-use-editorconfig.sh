25a6588d1dfe1a278e6e8ddb88e4b6c52c9ca0aa
Jason Jackson <jasonjackson@pobox.com>
2020-06-13 23:35:35 -0600

    Write tests for the use-editorconfig function

--- tests/test-use-editorconfig.sh
+++ tests/test-use-editorconfig.sh
@@ -5,12 +5,68 @@ script_dir="$(dirname -- "$0")"
 cd -- "$script_dir"
 source "../scripts/use-editorconfig.sh"
 
-# FIXME write tests
+function create-editorconfig-file() {
+  echo "root=true" > .editorconfig
+  echo "[*]" >> .editorconfig
 
-# .editorconfig in same directory
-# .editorconfig in parent directory
-# no .editorconfig
-# path doesn't exist, .editorconfig is present
-# path doesn't exist, no .editorconfig
-# /dev/null
-# path isn't absolute (editorconfig errors)
+  local line
+  for line in "$@"; do
+    echo "$line" >> .editorconfig
+  done
+}
+
+function run-test() {
+  local desc="$1" expected_style="$2" expected_size="$3"
+
+  [[ $first_test == false ]] && echo || first_test=false
+  echo -e "$desc"
+
+  use-editorconfig "a file.txt"
+	[[ -n $_indent_style ]] || _indent_style="-"
+	[[ -n $_indent_size ]] || _indent_size="-"
+
+  local color
+  [[ "$_indent_style" == "$expected_style" ]] && color="32m" || color="1;31m"
+  _indent_style="\033[${color}${_indent_style}\033[0m"
+  [[ "$_indent_size" == "$expected_size" ]] && color="32m" || color="1;31m"
+  _indent_size="\033[${color}${_indent_size}\033[0m"
+
+  echo -e "  indent style: $_indent_style"
+  echo -e "  indent size:  $_indent_size"
+}
+
+tmp_dir="$(mktemp -d)"
+mkdir "$tmp_dir/a directory"
+cd "$tmp_dir/a directory"
+touch "a file.txt"
+
+run-test ".editorconfig doesn't exist" - -
+run-test ".editorconfig doesn't exist (/dev/null)" - -
+
+create-editorconfig-file "indent_style=space"
+run-test "indent_style=space alone" space -
+
+create-editorconfig-file "indent_style=space" "indent_size=blah"
+run-test "indent_style=space, indent_size isn't valid" space -
+
+create-editorconfig-file "indent_style=space" "indent_size=2"
+run-test "indent_style=space, indent_size is valid" space 2
+
+create-editorconfig-file "indent_style=tab"
+run-test "indent_style=tab alone" tab -
+
+create-editorconfig-file "indent_style=tab" "tab_width=blah" "indent_size=blah"
+run-test "indent_style=tab, tab_width isn't valid, indent_size isn't valid" tab -
+
+create-editorconfig-file "indent_style=tab" "tab_width=blah" "indent_size=4"
+run-test "indent_style=tab, tab_width isn't valid, indent_size is valid" tab 4
+
+create-editorconfig-file "indent_style=tab" "tab_width=4" "indent_size=8"
+run-test "indent_style=tab, tab_width is valid, indent_size is valid & different" tab 4
+
+create-editorconfig-file "tab_width=8"
+run-test "indent_style isn't present, tab_width is valid" - 8
+
+cd
+rm -rf "$tmp_dir"
+echo "done"
