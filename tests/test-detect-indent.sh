54410100091a365b2ee56adb84fadf9b778b8454
Jason Jackson <jasonjackson@pobox.com>
2020-06-14 23:14:24 -0600

    Improve tests for indentation detection

--- tests/test-detect-indent.sh
+++ tests/test-detect-indent.sh
@@ -5,11 +5,34 @@ script_dir="$(dirname -- "$0")"
 cd -- "$script_dir"
 source "../scripts/detect-indent.sh"
 
-for fixture_file in not-there.txt fixtures/*; do
-	detect-indent "$fixture_file"  # Sets $_indent_style/$_indent_size
+function run-test() {
+  local fixture_file="$1" expected_style="$2" expected_size="$3"
 
+	detect-indent "$fixture_file"
 	[[ -n $_indent_style ]] || _indent_style="-"
 	[[ -z $_indent_size ]] || _indent_size="($_indent_size)"
 
-	echo "$(basename "$fixture_file"):"$'\t'"$_indent_style $_indent_size"
+  local color
+  [[ "$_indent_style" == "$expected_style" ]] && color="32m" || color="1;31m"
+  _indent_style="\033[${color}${_indent_style}\033[0m"
+  [[ "$_indent_size" == "$expected_size" ]] && color="32m" || color="1;31m"
+  _indent_size="\033[${color}${_indent_size}\033[0m"
+
+	echo -e "$(basename "$fixture_file"):"$'\t'"$_indent_style $_indent_size"
+}
+
+for fixture_file in not-there.txt fixtures/*; do
+	digits=${fixture_file//[^0-9]/}
+	if [[ -n $digits && $fixture_file != *"UTF"* ]]; then
+		expected_style="space"
+		expected_size="($digits)"
+	elif [[ $fixture_file == *"tab"* ]]; then
+		expected_style="tab"
+		expected_size=""
+	else
+		expected_style="-"
+		expected_size=""
+	fi
+
+	run-test "$fixture_file" "$expected_style" "$expected_size"
 done
