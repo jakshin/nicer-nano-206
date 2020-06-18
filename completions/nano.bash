ec7133c3c20878669a1d08c2d22e7c67cf6793f8
Jason Jackson <jasonjackson@pobox.com>
2020-06-17 23:28:07 -0600

    Add bash completions for nano v2.0.6
    With detection of nano-smart-indent.sh as 'nano' via alias/function/symlink, and auto-completing the '--tabs' option if so.

---
+++ completions/nano.bash
@@ -0,0 +1,36 @@
+# Bash completions for nano v2.0.6
+# (Also for nano-smart-indent.sh's '--tabs' option)
+
+function _nano() {
+	local i
+	for (( i=COMP_CWORD-1; i >= 0; i-- )); do
+		# We only provide completions for options, so just bail if "--" was already passed
+		[[ ${COMP_WORDS[$i]} == "--" ]] && return
+	done
+
+	local current_word="${COMP_WORDS[COMP_CWORD]}"
+	if [[ $current_word == -* ]]; then
+		# Start with options supported by nano v2.0.6
+		local opts='--autoindent --backup --backupdir= --boldtext --const --cut --fill= --help
+								--historylog --ignorercfiles --morespace --mouse --multibuffer --noconvert
+								--nofollow --nohelp --nonewlines --noread --nowrap --operatingdir= --preserve
+								--quickblank --quotestr= --rebinddelete --rebindkeypad --restricted --smarthome
+								--smooth --speller= --suspend --syntax= --tabsize= --tabstospaces --tempfile
+								--version --view --wordbounds -A -B -c -C -d -D -E -F -h -H -i -I -k -K -l -L
+								-m -n -N -o -O -p -Q -r -R -s -S -t -T -U -v -V -w -W -x -Y -z'
+
+		# If "nano" is actually nano-smart-indent.sh, via alias/function/symlink, add a "--tabs" option
+		if [[ "$(type nano)" == *"nano-smart-indent"* ]]; then
+			opts+=' --tabs'
+		else
+			local nano_cmd="$(type -p nano)"
+			if [[ -L $nano_cmd && "$(readlink "$nano_cmd")" == *"nano-smart-indent.sh"* ]]; then
+				opts+=' --tabs'
+			fi
+		fi
+
+		COMPREPLY=( $(compgen -W "$opts" -- "$current_word") )
+	fi
+}
+
+complete -F _nano -o bashdefault -o default nano
